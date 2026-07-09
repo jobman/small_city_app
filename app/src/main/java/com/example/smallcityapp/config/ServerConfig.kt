4fd927ab6815c8878c4c5a029b59d2ff3b2d36fd
Oleksii SHCHELKONOHOV <jobmanalex@gmail.com>
2026-07-09 15:37:40 +0300

    Use production HTTPS server

--- app/src/main/java/com/example/smallcityapp/config/ServerConfig.kt
+++ app/src/main/java/com/example/smallcityapp/config/ServerConfig.kt
@@ -1,5 +1,5 @@
 package com.example.smallcityapp.config
 
 object ServerConfig {
-    const val BASE_URL = "http://16.16.144.225/"
+    const val BASE_URL = "https://digital.trostyanets-miskrada.gov.ua/"
 }
